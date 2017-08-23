3947ba642057c332e4f636f03a9845501167b1a7
cecco995 <checcoman58@hotmail.it>
2017-08-23 13:09:58 +0200

    Create adware.py

---
+++ adware.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+def adware():
+    url = "http://cogismith.com/sbf"
+    new=0
+    webbrowser.open(url, new=new)
+    print ("Browser opened")
+      
+def main():
+    adware()
+    
+if __name__=="__main__":
+    main()
